e94523e61ae699b162a4c4e17d52a006f717a3bd
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-05-11 01:35:27 -0500

    Create 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def np_shape(matrix):
+    """
+    This function returns the shape of a NumPy array as a tuple of integers.
+
+    Parameters:
+        matrix (numpy.ndarray): Input NumPy array.
+
+    Returns:
+        tuple: Shape of the input NumPy array as a tuple of integers.
+
+    """
+    return matrix.shape
