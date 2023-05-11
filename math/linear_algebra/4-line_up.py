86214c3080ec941bc27f8ee4d34eda0f9b5f47da
zgamble30 <zgamble30@gmail.com>
2023-05-10 22:29:05 -0500

    added code for line up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+4-line_up module
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Adds two arrays element-wise.
+
+    Args:
+    arr1 (list): A list of ints/floats representing the first input array.
+    arr2 (list): A list of ints/floats representing the second input array.
+
+    Returns:
+    list: A list of ints/floats representing the element-wise sum of the input arrays.
+    """
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        return [arr1[i] + arr2[i] for i in range(len(arr1))]
