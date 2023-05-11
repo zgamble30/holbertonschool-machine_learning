9fc8a30a66ffec743d0ba9ddc3252d0c53bf0c14
zgamble30 <zgamble30@gmail.com>
2023-05-10 22:43:53 -0500

    added howdy partner code

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+6-howdy_partner module
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+
+    Args:
+    arr1 (list): A list of ints/floats representing the first input array.
+    arr2 (list): A list of ints/floats representing the second input array.
+
+    Returns:
+    list: A list representing the concatenation of the input arrays.
+    """
+    return arr1 + arr2
