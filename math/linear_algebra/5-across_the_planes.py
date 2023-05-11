a13d6a1e20f05e8b61e0e531b883a5dd434a7846
zgamble30 <zgamble30@gmail.com>
2023-05-10 22:33:53 -0500

    added code for across the planes

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+5-across_the_planes module
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two matrices element-wise.
+
+    Args:
+    mat1 (list): A nested list representing the first input matrix.
+    mat2 (list): A nested list representing the second input matrix.
+
+    Returns:
+    list: A nested list representing the element-wise sum of the input matrices.
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    else:
+        return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
