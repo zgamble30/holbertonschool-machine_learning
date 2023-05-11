6d6f6234e1c19988e1a65387d95300e147a0db84
zgamble30 <zgamble30@gmail.com>
2023-05-10 22:37:14 -0500

    i keep making lines too long on accident

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -18,4 +18,7 @@ def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     else:
-        return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+        return [
+            [mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+            for i in range(len(mat1))
+        ]
