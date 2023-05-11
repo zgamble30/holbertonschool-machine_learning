9b3dab987d853b2b3487424f5155416fe7901a1e
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-05-11 00:25:05 -0500

    Update 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -3,7 +3,7 @@ def mat_mul(mat1, mat2):
     """
     Multiplies two matrices mat1 and mat2.
     Args: mat1, mat2
-    Returns: "OK" string
+    Returns: new matrix
     """
     # Get dimensions of matrices
     rows1, cols1 = len(mat1), len(mat1[0])
@@ -11,7 +11,7 @@ def mat_mul(mat1, mat2):
 
     # Check if matrices can be multiplied
     if cols1 != rows2:
-        return "OK"
+        return None
 
     # Create a new matrix to store the result
     result = [[0 for _ in range(cols2)] for _ in range(rows1)]
@@ -22,4 +22,5 @@ def mat_mul(mat1, mat2):
             for k in range(rows2):
                 result[i][j] += mat1[i][k] * mat2[k][j]
 
-    return "OK"
+    return result
+
