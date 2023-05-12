8a307e6dce1c0e404bcfe7298b6c04b7e695dfb0
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-05-12 00:58:54 -0500

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
 
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# Plotting the histogram
+plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
+# Setting the labels and title
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+
+# Display the plot
+plt.show()
