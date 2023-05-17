c857df2e5b512478fb7ae39af4c7d0a44cc20593
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-05-17 15:57:30 -0500

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,27 +1,14 @@
 #!/usr/bin/env python3
-"""
-Module to calculate the derivative of a polynomial.
-"""
+""" 10-matisse """
+
 
 def poly_derivative(poly):
-    """
-    Calculates the derivative of a polynomial.
-
-    Args:
-    poly (list): A list of coefficients representing a polynomial.
-                 The index of the list represents the power of x that the coefficient belongs to.
-
-    Returns:
-    list: A new list of coefficients representing the derivative of the polynomial.
-          Returns None if poly is not valid. If the derivative is 0, returns [0].
-    """
-
-    # Check if poly is a valid input
-    if not isinstance(poly, list) or len(poly) < 2:
+    """ find derivative"""
+    if type(poly) != list or len(poly) == 0:
         return None
-
-    # Calculate the derivative of the polynomial
-    derivative = [i * poly[i] for i in range(1, len(poly))]
-
-    # Return the calculated derivative or [0] if the derivative is all zeros
-    return derivative if any(derivative) else [0]
+    if len(poly) == 1:
+        return [0]
+    for x in range(1, len(poly)):
+        poly[x] = poly[x] * x
+    poly.pop(0)
+    return poly
