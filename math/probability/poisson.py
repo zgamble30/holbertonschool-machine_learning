065089f0bbfdc1e047c97af883df6a14a1bf1d7f
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-05-24 17:12:39 -0500

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,69 +1,50 @@
 #!/usr/bin/env python3
+"""Poisson class for distribution"""
+
 
 class Poisson:
     """
-    Class representing a Poisson distribution.
+    Poisson class for distribution
     """
 
     def __init__(self, data=None, lambtha=1.):
-        """
-        Initializes a Poisson distribution.
-
-        Args:
-            data (list): List of data points to estimate the distribution.
-            lambtha (float): Expected number of occurrences in a given time frame.
-
-        Raises:
-            ValueError: If lambtha is not a positive value or equals 0.
-            TypeError: If data is not a list.
-            ValueError: If data does not contain at least two data points.
-        """
-
-        if data is None:
-            if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
-        else:
-            if not isinstance(data, list):
+        self.lambtha = float(lambtha)
+        if self.lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is not None:
+            if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
-
-    @staticmethod
-    def factorial(n):
-        """
-        Computes the factorial of a number.
-
-        Args:
-            n (int): Number to compute the factorial of.
-
-        Returns:
-            int: Factorial of the number.
-        """
-
-        if n == 0 or n == 1:
-            return 1
-        else:
-            return n * Poisson.factorial(n - 1)
+            self.lambtha = float(sum(data)) / len(data)
 
     def pmf(self, k):
         """
-        Calculates the value of the PMF for a given number of successes.
-
-        Args:
-            k (int): Number of successes.
-
-        Returns:
-            float: Probability mass function (PMF) value.
+        Calculates the value of the PMF for a given number of successes
         """
-        if not isinstance(k, int) or k < 0:
+
+        if type(k) is not int:
+            k = int(k)
+        if (k < 0):
             return 0
-
-        e_approx = 2.7182818285  # Approximation for the value of e
+        e = 2.7182818285
         lambtha = self.lambtha
-        k = int(k)
+        factorial = 1
+        for i in range(k):
+            factorial *= (i + 1)
+        pmf = ((lambtha ** k) * (e ** - lambtha)) / factorial
+        return pmf
 
-        pmf_value = (lambtha ** k) * (e_approx ** (-lambtha))
-        pmf_value /= Poisson.factorial(k)
-        return pmf_value
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of successes
+        """
+
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
