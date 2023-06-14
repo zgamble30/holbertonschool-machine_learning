99e4cd7d67e6d6c69b0ba9ba68ea767364d17eb9
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-06-14 14:38:05 -0500

    Create 2-forward_prop.py

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" task 2 """
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    fonction de propagation
+    :param x: int
+    :param activation:list
+    :return:
+    """
+    create_layer = __import__('1-create_layer').create_layer
+    hmed = create_layer(x, layer_sizes[0], activations[0])
+    for i in range(1, len(layer_sizes)):
+        hmed = create_layer(hmed, layer_sizes[i], activations[i])
+    return (hmed)
