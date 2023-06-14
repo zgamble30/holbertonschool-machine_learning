92b43071fa9ae8b341da35b492cf2de62ee1418d
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-06-14 14:39:56 -0500

    Create 4-calculate_loss.py

---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" task 4 """
+import tensorflow as tf
+
+
+def calculate_loss(y, y_pred):
+    """
+    calcule de perte
+    :param y: espace res
+    :param y_pred: le estim
+    """
+    zok_secretaire = tf.losses.softmax_cross_entropy(y, logits=y_pred,
+                                                     weights=1.0, scope=None)
+    return (zok_secretaire)
