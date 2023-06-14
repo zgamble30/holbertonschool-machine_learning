8061b29cb697d6759f8e89edb23d086f9fc3e645
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-06-14 14:42:08 -0500

    Update 2-forward_prop.py

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
-""" task 2 """
+"""Forward propagation"""
+import tensorflow as tf
+
+create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
+    """Creates the forward propagation graph for the neural network.
+    Args:
+        x (tensor): placeholder for the input data.
+        layer_sizes (list): list containing the number of nodes in each layer
+                            of the network.
+        activations (list): list containing the activation functions for each
+                            layer of the network.
+    Returns:
+        tensor: prediction of neural network.
     """
-    fonction de propagation
-    :param x: int
-    :param activation:list
-    :return:
-    """
-    create_layer = __import__('1-create_layer').create_layer
-    hmed = create_layer(x, layer_sizes[0], activations[0])
-    for i in range(1, len(layer_sizes)):
-        hmed = create_layer(hmed, layer_sizes[i], activations[i])
-    return (hmed)
+    for layer in range(0, len(layer_sizes)):
+        if layer == 0:
+            A = create_layer(x, layer_sizes[layer], activations[layer])
+        else:
+            A = create_layer(A, layer_sizes[layer], activations[layer])
+    return A
