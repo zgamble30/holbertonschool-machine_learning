ea794fd12989a029e1f4f05783fb029a17abaa7c
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-06-16 09:35:14 -0500

    Update 14-neural_network.py

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -1,100 +1,151 @@
 #!/usr/bin/env python3
-""" neural network class"""
+"""
+Module that defines a neural network with one hidden layer
+performing binary classification
+"""
+
 import numpy as np
 
 
 class NeuralNetwork:
-    """ Creating Neural Network class"""
+    """
+    NeuralNetwork class
+    """
 
     def __init__(self, nx, nodes):
-        """ initialize class"""
-        if type(nx) != int:
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if type(nodes) != int:
-            raise TypeError("nodes must be an integer")
-        if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
-        self.__W1 = np.random.normal(size=(nodes, nx))
-        self.__b1 = np.zeros((nodes, 1))
+        """
+        Constructor method
+        """
+
+        self.nx = nx
+        self.nodes = nodes
+
+        self.__W1 = np.random.randn(self.nodes, self.nx)
+        self.__b1 = np.zeros((self.nodes, 1))
         self.__A1 = 0
-        self.__W2 = np.random.normal(size=(1, nodes))
+
+        self.__W2 = np.random.randn(1, self.nodes)
         self.__b2 = 0
         self.__A2 = 0
 
     @property
     def W1(self):
-        """ return private w1"""
+        """
+        Getter method for W1
+        """
         return self.__W1
 
     @property
     def b1(self):
-        """ return private b1"""
+        """
+        Getter method for b1
+        """
         return self.__b1
 
     @property
     def A1(self):
-        """ return private A1"""
+        """
+        Getter method for A1
+        """
         return self.__A1
 
     @property
     def W2(self):
-        """ return private w2"""
+        """
+        Getter method for W2
+        """
         return self.__W2
 
     @property
     def b2(self):
-        """ return private b2"""
+        """
+        Getter method for b2
+        """
         return self.__b2
 
     @property
     def A2(self):
-        """ return private a2"""
+        """
+        Getter method for A2
+        """
         return self.__A2
 
     def forward_prop(self, X):
-        """ set A with forward prop"""
-        z1 = np.matmul(self.__W1, X) + self.__b1
-        self.__A1 = 1/(1 + np.exp(-z1))
-        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
-        self.__A2 = 1/(1 + np.exp(-z2))
+        """
+        Method that calculates the forward propagation
+        """
+
+        self.__A1 = 1 / (1 + np.exp(-(
+            np.matmul(self.__W1, X) + self.__b1)))
+        self.__A2 = 1 / (1 + np.exp(-(
+            np.matmul(self.__W2, self.__A1) + self.__b2)))
+
         return self.__A1, self.__A2
 
-    def cost(self, Y, A):
-        """ return the cost """
-        cost = -(Y * np.log(A) + (1 - Y) * (np.log(1.0000001 - A))).mean()
-        return cost
-
-    def evaluate(self, X, Y):
-        """ evaluate neuron prediction """
-        self.forward_prop(X)
-        return np.round(self.__A2).astype(int), self.cost(Y, self.__A2)
-
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
-        """ calculate gradient descent"""
-        m = Y.shape[1]
-        dz2 = A2 - Y
-        dw2 = np.matmul(dz2, A1.T) / m
-        db2 = dz2.mean(axis=1, keepdims=True)
-        dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
-        dw1 = np.matmul(dz1, X.T) / m
-        db1 = dz1.mean(axis=1, keepdims=True)
-        self.__W1 -= alpha * dw1
-        self.__b1 -= alpha * db1
-        self.__W2 -= alpha * dw2
+        """
+        Calculates one pass of gradient descent on the neural network
+        """
+
+        m = X.shape[1]
+
+        dZ2 = A2 - Y
+        dW2 = np.dot(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+
+        dZ1 = np.dot(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = np.dot(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+        self.__W2 -= alpha * dW2
         self.__b2 -= alpha * db2
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
-        """ train model based on gradient descent"""
-        if type(iterations) != int:
+        """
+        Trains the neural network
+        """
+
+        if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-        if type(alpha) != float:
+        if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        for _ in range(iterations):
-            self.gradient_descent(X, Y, *self.forward_prop(X), alpha)
-        return self.evaluate(X, Y)
+
+        for i in range(iterations):
+            _, A2 = self.forward_prop(X)
+                   self.gradient_descent(X, Y, self.__A1, A2, alpha)
+
+    _, cost = self.forward_prop(X)
+    cost = np.squeeze(cost)
+    predictions = np.where(self.__A2 >= 0.5, 1, 0)
+    accuracy = np.mean(predictions == Y) * 100
+
+    return predictions, cost
+
+def evaluate(self, X, Y):
+    """
+    Evaluates the neural network's predictions
+    """
+
+    A1, A2 = self.forward_prop(X)
+    cost = self.cost(Y, A2)
+    predictions = np.where(A2 >= 0.5, 1, 0)
+
+    return predictions, cost
+
+def cost(self, Y, A):
+    """
+    Calculates the cost of the model using logistic regression
+    """
+
+    m = Y.shape[1]
+    cost = (-1 / m) * np.sum(
+        Y * np.log(A) + (1 - Y) * np.log(1 - A))
+
+    return cost
+
