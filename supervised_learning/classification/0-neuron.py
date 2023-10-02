ffe0d8f81318d2b601239c9ee9089cae50e340be
zgamble30 <zgamble30@gmail.com>
2023-10-02 17:28:08 -0500

    attempting to fix checker error

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -8,7 +8,7 @@ class Neuron:
     def __init__(self, nx):
         """
         Initialize a neuron with random normal weights and a bias.
-        I used the paramaters given to me by the assignment.
+        I used the parameters given to me by the assignment.
 
         Parameters:
         - nx: The number of input features to the neuron.
@@ -25,4 +25,5 @@ class Neuron:
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
-        """set to 0"""
+
+# Added a newline here to try and fix checker
