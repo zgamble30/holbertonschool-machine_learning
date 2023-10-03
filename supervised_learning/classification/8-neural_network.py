9c72d8439a236694bcb0138e00ef45390f063ee2
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-10-02 20:10:22 -0500

    Update 8-neural_network.py

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,41 +1,27 @@
 #!/usr/bin/env python3
-"""Neural Network with one hidden layer for binary classification"""
+"""Neural Network"""
 
 import numpy as np
 
 
 class NeuralNetwork:
-    """
-    Defines a neural network with one hidden layer for binary classification.
-    """
+    """defines a neural network with one hidden layer
+    performing binary classification"""
 
     def __init__(self, nx, nodes):
-        """
-        Initializes the neural network.
-
-        Args:
-            - nx (int): Number of input features.
-            - nodes (int): Number of nodes in the hidden layer.
-
-        Raises:
-            - TypeError: If nx or nodes is not an integer.
-            - ValueError: If nx or nodes is less than 1.
-        """
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if not isinstance(nodes, int):
-            raise TypeError("nodes must be an integer")
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
         if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
+            raise ValueError('nodes must be a positive integer')
 
-        # Initialize weights and biases for the hidden layer
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros(shape=(nodes, 1))
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-
-        # Initialize weights and biases for the output neuron
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
