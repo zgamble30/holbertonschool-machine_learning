0426409a000a54cd4cd1a6615b688403ec36e989
zgamble30 <zgamble30@gmail.com>
2023-10-02 19:57:30 -0500

    reworked the code trying to get checks

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
-"""Neural network with one hidden layer performing binary classification"""
+"""Neural Network with one hidden layer for binary classification"""
 
 import numpy as np
 
 
 class NeuralNetwork:
-    """Defines a neural network with one hidden layer performing binary classification"""
+    """
+    Defines a neural network with one hidden layer for binary classification.
+    """
 
     def __init__(self, nx, nodes):
         """
-        Initializes the NeuralNetwork object.
+        Initializes the neural network.
 
-        Parameters:
-        - nx: The number of input features.
-        - nodes: The number of nodes found in the hidden layer.
+        Args:
+            - nx (int): Number of input features.
+            - nodes (int): Number of nodes in the hidden layer.
 
         Raises:
-        - TypeError: If nx is not an integer or if nodes is not an integer.
-        - ValueError: If nx is less than 1 or if nodes is less than 1.
+            - TypeError: If nx or nodes is not an integer.
+            - ValueError: If nx or nodes is less than 1.
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -30,7 +32,7 @@ class NeuralNetwork:
 
         # Initialize weights and biases for the hidden layer
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
+        self.b1 = np.zeros(shape=(nodes, 1))
         self.A1 = 0
 
         # Initialize weights and biases for the output neuron
