41271268feb99659cb3866546151156770c2ab90
zgamble30 <zgamble30@gmail.com>
2023-10-02 20:20:59 -0500

    added private instance attributes

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""Neural Network"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Defines a neural network with one hidden layer
+    performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """
+        Class constructor to initialize the neural network.
+
+        Args:
+            nx (int): Number of input features.
+            nodes (int): Number of nodes in the hidden layer.
+
+        Raises:
+            TypeError: If nx or nodes is not an integer.
+            ValueError: If nx or nodes is less than 1.
+        """
+        # Check if nx is an integer and positive
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        # Check if nodes is an integer and positive
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        # Initialize weights and biases for the hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Initialize weights and biases for the output neuron
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # Getter functions for private attributes
+    @property
+    def W1(self):
+        """Getter for W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for A2"""
+        return self.__A2
