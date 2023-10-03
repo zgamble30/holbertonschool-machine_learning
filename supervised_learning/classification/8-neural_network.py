5cadebc54dac7220766d4e39093d1f1c0d58525c
zgamble30 <zgamble30@gmail.com>
2023-10-02 20:16:12 -0500

    added more comments

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -3,25 +3,40 @@
 
 import numpy as np
 
-
 class NeuralNetwork:
-    """defines a neural network with one hidden layer
+    """Defines a neural network with one hidden layer
     performing binary classification"""
 
     def __init__(self, nx, nodes):
-        """Class constructor"""
+        """
+        Class constructor to initialize the neural network.
+
+        Args:
+            nx (int): Number of input features.
+            nodes (int): Number of nodes in the hidden layer.
+
+        Raises:
+            TypeError: If nx or nodes is not an integer.
+            ValueError: If nx or nodes is less than 1.
+        """
+        # Check if nx is an integer and positive
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
+
+        # Check if nodes is an integer and positive
         if type(nodes) is not int:
             raise TypeError('nodes must be an integer')
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
 
+        # Initialize weights and biases for the hidden layer
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
+
+        # Initialize weights and biases for the output neuron
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
