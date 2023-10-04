0061617e424834f58dc4e942c7ff6928066e89c5
zgamble30 <zgamble30@gmail.com>
2023-10-04 00:07:40 -0500

    added example

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -1,36 +1,56 @@
 #!/usr/bin/env python3
-"""Module for decoding a one-hot matrix into labels."""
+"""Module for decoding one-hot encoded matrices into labels."""
 import numpy as np
 
-def one_hot_decode(one_hot):
+def one_hot_decode(one_hot_matrix):
     """
-    Convert a one-hot matrix into a vector of labels.
+    Convert a one-hot encoded matrix into a vector of class labels.
 
     Args:
-        one_hot (numpy.ndarray): One-hot encoded matrix with shape (num_classes, num_examples).
-            num_classes (int): Maximum number of classes.
-            num_examples (int): Number of examples.
+        one_hot_matrix (numpy.ndarray): A matrix with shape (num_classes, num_examples),
+            where each column represents a one-hot encoded class.
 
     Returns:
-        numpy.ndarray: Vector of labels with shape (num_examples, ), or None on failure.
+        numpy.ndarray: A vector with shape (num_examples, ) containing numeric labels,
+            or None if the input is invalid.
     """
-    # Ensure that one_hot is a valid numpy array with shape (num_classes, num_examples)
-    if not isinstance(one_hot, np.ndarray) or one_hot.ndim != 2:
+    # Check if the input is a valid numpy array with shape (num_classes, num_examples)
+    if not isinstance(one_hot_matrix, np.ndarray) or one_hot_matrix.ndim != 2:
         return None
 
     # Get the number of examples (num_examples)
-    num_examples = one_hot.shape[1]
+    num_examples = one_hot_matrix.shape[1]
 
     # Initialize an empty label vector
-    labels = np.zeros((num_examples,), dtype=int)
+    label_vector = np.zeros((num_examples,), dtype=int)
 
-    # Iterate over the examples
+    # Iterate over each example
     for i in range(num_examples):
         # Find the index of the maximum value in each example (column-wise)
-        max_index = np.argmax(one_hot[:, i])
+        max_index = np.argmax(one_hot_matrix[:, i])
 
         # Set the corresponding label to the maximum index
-        labels[i] = max_index
+        label_vector[i] = max_index
 
     # Return the resulting label vector
-    return labels
+    return label_vector
+
+"""Example usage:
+if __name__ == "__main__":
+    import numpy as np
+
+    # Sample one-hot encoded matrix (replace this with your own data)
+    one_hot_data = np.array([[0, 1, 0, 0],
+                             [1, 0, 0, 0],
+                             [0, 0, 1, 0],
+                             [0, 0, 0, 1]])
+
+    # Decode one-hot encoded matrix
+    decoded_labels = one_hot_decode(one_hot_data)
+
+    # Display results
+    print("One-Hot Encoded Matrix:")
+    print(one_hot_data)
+    print("\nDecoded Labels:")
+    print(decoded_labels)
+    """
