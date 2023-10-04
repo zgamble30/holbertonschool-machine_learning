c42faa10d6c4f560723e97acd944e5e74ab16fe7
zgamble30 <zgamble30@gmail.com>
2023-10-03 23:04:45 -0500

    switching the approach.

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,107 +1,52 @@
 #!/usr/bin/env python3
-"""Deep neural network module"""
+"""Deep Neural Network class"""
 
 import numpy as np
 
+
 class DeepNeuralNetwork:
-    """Defines a deep neural network performing binary classification."""
+    """Class defining a deep neural network for binary classification."""
+
+    @staticmethod
+    def initialize_weights(nx, layers):
+        """
+        Initializes weights using the He et al. method.
+
+        Args:
+            nx (int): Number of input features.
+            layers (list): List representing the number of nodes in each layer.
+
+        Returns:
+            dict: Dictionary containing initialized weights and biases.
+        """
+        weights = dict()
+        for i in range(len(layers)):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError('layers must be a list of positive integers')
+            prev_layer = layers[i - 1] if i > 0 else nx
+            w_part1 = np.random.randn(layers[i], prev_layer)
+            w_part2 = np.sqrt(2 / prev_layer)
+            weights.update({
+                'b' + str(i + 1): np.zeros((layers[i], 1)),
+                'W' + str(i + 1): w_part1 * w_part2
+            })
+        return weights
 
     def __init__(self, nx, layers):
-        """Class constructor"""
-        if type(nx) is not int:
+        """
+        Class constructor.
+
+        Args:
+            nx (int): Number of input features.
+            layers (list): List representing the number of nodes in each layer.
+        """
+        if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        if not all(isinstance(i, int) and i > 0 for i in layers) or not layers:
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
 
-        self.__L, self.__cache, self.__weights = len(layers), {}, {}
-        for l in range(self.L):
-            W, b = 'W' + str(l + 1), 'b' + str(l + 1)
-            if l == 0:
-                self.weights[W] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
-            else:
-                self.weights[W] = np.random.randn(layers[l], layers[l - 1]) * np.sqrt(2 / layers[l - 1])
-            self.weights[b] = np.zeros((layers[l], 1))
-
-    @property
-    def L(self):
-        """Getter method for L (number of layers)."""
-        return self.__L
-
-    @property
-    def cache(self):
-        """Getter method for cache (intermediary values)."""
-        return self.__cache
-
-    @property
-    def weights(self):
-        """Getter method for weights (weights and biases)."""
-        return self.__weights
-
-    def forward_prop(self, X):
-        """Calculates the forward propagation of the neural network."""
-        self.cache['A0'] = X
-        for l in range(1, self.L + 1):
-            W, b, A, new_A = 'W' + str(l), 'b' + str(l), 'A' + str(l - 1), 'A' + str(l)
-            Z = np.dot(self.weights[W], self.cache[A]) + self.weights[b]
-            self.cache[new_A] = 1 / (1 + np.exp(-Z))
-        return self.cache[new_A], self.cache
-#!/usr/bin/env python3
-"""Deep neural network module"""
-
-import numpy as np
-
-class DeepNeuralNetwork:
-    """Defines a deep neural network performing binary classification."""
-
-    def __init__(self, nx, layers):
-        """Class constructor"""
-        if type(nx) is not int:
-            raise TypeError('nx must be an integer')
-        if nx < 1:
-            raise ValueError('nx must be a positive integer')
-        if not all(isinstance(i, int) and i > 0 for i in layers) or not layers:
-            raise TypeError('layers must be a list of positive integers')
-
-        self.__L, self.__cache, self.__weights = len(layers), {}, {}
-        for l in range(self.L):
-            W, b = 'W' + str(l + 1), 'b' + str(l + 1)
-            if l == 0:
-                self.weights[W] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
-            else:
-                self.weights[W] = np.random.randn(layers[l], layers[l - 1]) * np.sqrt(2 / layers[l - 1])
-            self.weights[b] = np.zeros((layers[l], 1))
-
-    @property
-    def L(self):
-        """Getter method for L (number of layers)."""
-        return self.__L
-
-    @property
-    def cache(self):
-        """Getter method for cache (intermediary values)."""
-        return self.__cache
-
-    @property
-    def weights(self):
-        """Getter method for weights (weights and biases)."""
-        return self.__weights
-
-    @L.setter
-    def L(self, value):
-        """Setter method for L (number of layers)."""
-        if type(value) is not int:
-            raise TypeError('L must be an integer')
-        if value < 1:
-            raise ValueError('L must be a positive integer')
-        self.__L = value
-
-    def forward_prop(self, X):
-        """Calculates the forward propagation of the neural network."""
-        self.cache['A0'] = X
-        for l in range(1, self.L + 1):
-            W, b, A, new_A = 'W' + str(l), 'b' + str(l), 'A' + str(l - 1), 'A' + str(l)
-            Z = np.dot(self.weights[W], self.cache[A]) + self.weights[b]
-            self.cache[new_A] = 1 / (1 + np.exp(-Z))
-        return self.cache[new_A], self.cache
+        self.L = len(layers)
+        self.cache = dict()
+        self.weights = self.initialize_weights(nx, layers)
