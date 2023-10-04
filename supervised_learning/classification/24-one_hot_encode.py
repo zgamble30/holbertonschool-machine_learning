dc795f4604afadfdebb27e6f79a4b0dc24f796e7
zgamble30 <zgamble30@gmail.com>
2023-10-03 23:51:48 -0500

    improve readability

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -1,42 +1,40 @@
 #!/usr/bin/env python3
-"""Provides a function to convert numeric l
-abel vector into a one-hot matrix."""
+"""Module for converting numeric labels into a one-hot matrix."""
 import numpy as np
 
-
-def one_hot_encode(Y, classes):
+def one_hot_encode(labels, num_classes):
     """
-    Converts a numeric label vector into a one-hot matrix.
+    Convert numeric labels into a one-hot matrix.
 
     Args:
-        Y (numpy.ndarray): Numeric label vector with shape (m,).
-        classes (int): Maximum number of classes found in Y.
+        labels (numpy.ndarray): Numeric label vector with shape (num_examples,).
+        num_classes (int): Maximum number of classes present in labels.
 
     Returns:
-        numpy.ndarray: One-hot encoding of Y with shape (classes, m),
-                      or None on failure.
+        numpy.ndarray: One-hot encoding of labels with shape (num_classes, num_examples),
+                      or None if the input is invalid.
     """
-    # Check if Y is a valid numpy array with shape (m,)
-    if not isinstance(Y, np.ndarray) or Y.ndim != 1:
+    # Ensure that labels is a valid numpy array with shape (num_examples,)
+    if not isinstance(labels, np.ndarray) or labels.ndim != 1:
         return None
 
-    # Check if classes is an integer
-    if not isinstance(classes, int):
+    # Ensure that num_classes is an integer
+    if not isinstance(num_classes, int):
         return None
 
-    # Get the number of examples (m)
-    m = Y.shape[0]
+    # Get the number of examples (num_examples)
+    num_examples = labels.shape[0]
 
-    # Create an array of zeros with shape (classes, m)
-    one_hot = np.zeros((classes, m))
+    # Create an array of zeros with shape (num_classes, num_examples)
+    one_hot_matrix = np.zeros((num_classes, num_examples))
 
-    # Iterate over the label vector Y
-    for i, label in enumerate(Y):
+    # Iterate over the label vector labels
+    for example_idx, label in enumerate(labels):
         # Check if the label is within the valid range
-        if label < 0 or label >= classes:
+        if label < 0 or label >= num_classes:
             return None
-        # Set the corresponding element in one_hot to 1
-        one_hot[label, i] = 1
+        # Set the corresponding element in one_hot_matrix to 1
+        one_hot_matrix[label, example_idx] = 1
 
     # Return the resulting one-hot encoding
-    return one_hot
+    return one_hot_matrix
