8a29af435281d97bf50bec6e692f1e53dbbc41b7
zgamble30 <zgamble30@gmail.com>
2023-10-04 11:31:30 -0500

    trying to fix errors. taking a different approach

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,79 +1,64 @@
 #!/usr/bin/env python3
-"""Deep Neural Network class"""
-
 import numpy as np
 
 
-class DeepNeuralNetwork:
-    """Class defining a deep neural network for binary classification."""
-
-    @staticmethod
-    def initialize_weights(nx, layers):
+class NeuralNetwork:
+    """
+    Defines a neural network for binary classification.
+    """
+    def __init__(self, input_size, layer_sizes):
         """
-        Initializes weights using the He et al. method.
+        Initializes the neural network.
 
-        Args:
-            nx (int): Number of input features.
-            layers (list): List representing the number of nodes in each layer.
+        Parameters:
+        - input_size (int): Number of input features.
+        - layer_sizes (list): List of positive integers representing the
+          number of nodes in each hidden layer.
 
-        Returns:
-            dict: Dictionary containing initialized weights and biases.
+        Raises:
+        - TypeError: If input_size is not an integer, or if layer_sizes is not
+          a list of positive integers.
+        - ValueError: If input_size is not a positive integer.
         """
-        weights = dict()
-        for i in range(1, len(layers) + 1):
-            # Check if nodes is an integer and positive
-            if not isinstance(layers[i - 1], int) or layers[i - 1] < 1:
-                raise TypeError('layers must be a list of positive integers')
-            
-            prev_nodes = nx if i == 1 else layers[i - 2]
-            weight_matrix = np.random.randn(layers[i - 1], prev_nodes)
-            weight_scaling = np.sqrt(2 / prev_nodes)
-            weights.update({
-                'b' + str(i): np.zeros((layers[i - 1], 1)),
-                'W' + str(i): weight_matrix * weight_scaling
-            })
-        return weights
+        if not isinstance(input_size, int):
+            raise TypeError("input_size must be an integer")
+        if input_size < 1:
+            raise ValueError("input_size must be a positive integer")
+        if not isinstance(layer_sizes, list) or len(layer_sizes) == 0:
+            raise TypeError("layer_sizes must be a list of positive integers")
+        if not all(map(lambda x: x > 0 and isinstance(x, int), layer_sizes)):
+            raise TypeError("layer_sizes must be a list of positive integers")
 
-    def __init__(self, nx, layers):
-        """
-        Class constructor.
+        self.__input_size = input_size
+        self.__layer_sizes = layer_sizes
+        self.__num_layers = len(layer_sizes)
+        self.__cache = {}
+        self.__weights = {}
 
-        Args:
-            nx (int): Number of input features.
-            layers (list): List representing the number of nodes in each layer.
-        """
-        # Check if nx is an integer
-        if not isinstance(nx, int):
-            raise TypeError('nx must be an integer')
-
-        # Check if nx is a positive integer
-        if nx < 1:
-            raise ValueError('nx must be a positive integer')
-
-        # Check if layers is a list
-        if not isinstance(layers, list):
-            raise TypeError('layers must be a list of positive integers')
-
-        # Check if elements in layers are positive integers
-        if not all(isinstance(nodes, int) and nodes > 0 for nodes in layers):
-            raise TypeError('layers must be a list of positive integers')
-
-        # Set private instance attributes
-        self.__L = len(layers)
-        self.__cache = dict()
-        self.__weights = self.initialize_weights(nx, layers)
+        for i in range(self.__num_layers):
+            if i == 0:
+                self.__weights['W' + str(i + 1)] = np.random.randn(layer_sizes[i], input_size) * np.sqrt(2 / input_size)
+            else:
+                self.__weights['W' + str(i + 1)] = np.random.randn(layer_sizes[i], layer_sizes[i - 1]) * np.sqrt(2 / layer_sizes[i - 1])
+            self.__weights['b' + str(i + 1)] = np.zeros((layer_sizes[i], 1))
 
     @property
-    def L(self):
-        """Getter for the number of layers"""
-        return self.__L
+    def num_layers(self):
+        """
+        Returns the number of layers in the neural network.
+        """
+        return self.__num_layers
 
     @property
     def cache(self):
-        """Getter for the cache"""
+        """
+        Returns the cache dictionary containing intermediary values.
+        """
         return self.__cache
 
     @property
     def weights(self):
-        """Getter for the weights"""
+        """
+        Returns the weights dictionary containing weights and biases.
+        """
         return self.__weights
