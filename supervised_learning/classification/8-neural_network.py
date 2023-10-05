d7fe065ecaaaea9ee8d60e2a55d50356b05272a6
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-10-04 19:22:19 -0500

    Update 8-neural_network.py

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python3
-"""creating nodes in a network.... networking....a neural network"""
+"""This is a neural network"""
+
 import numpy as np
 
 
 class NeuralNetwork:
-    """
-    Defines a neural network with one hidden layer performing binary
-    classification.
-    """
+    """Defines a binary classification neural
+    network with a single hidden layer."""
 
     def __init__(self, nx, nodes):
-        """
-        Class constructor to initialize a neural network.
-
-        nx: the number of input features to the neural network
-        nodes: the number of nodes found in the hidden layer
-
-        W1: The weights vector for the hidden layer. It is initialized using
-            a random normal distribution.
-        b1: The bias for the hidden layer. Initialized with 0’s.
-        A1: The activated output for the hidden layer. Initialized to 0.
-
-        W2: The weights vector for the output neuron. It is initialized using
-            a random normal distribution.
-        b2: The bias for the output neuron. Initialized to 0.
-        A2: The activated output for the output neuron (prediction).
-            Initialized to 0.
-        """
-
-        # Check if nx and nodes are of valid types and values
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if not isinstance(nodes, int):
-            raise TypeError("nodes must be an integer")
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
         if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
+            raise ValueError('nodes must be a positive integer')
 
-        # Initialize weights and biases for hidden layer and output neuron
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros(shape=(nodes, 1))
-        self.A1 = 0
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter method for W1 (weights of hidden layer)."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter method for b1 (biases of hidden layer)."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter method for A1 (activated output of hidden layer)."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter method for W2 (weights of output neuron)."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter method for b2 (bias of output neuron)."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter method for A2 (activated output of output neuron)."""
+        return self.__A2
