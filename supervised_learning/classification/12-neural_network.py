fc5a648ea41f3c0eb407d094be564e76904f2ffe
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-10-04 20:24:19 -0500

    Update 12-neural_network.py

--- supervised_learning/classification/12-neural_network.py
+++ supervised_learning/classification/12-neural_network.py
@@ -107,7 +107,66 @@ class NeuralNetwork:
             - Tuple of numpy.ndarray and float: Predicted labels and cost.
         """
         A, _ = self.forward_prop(X)
-        predictions = (A >= 0.5).astype(int).flatten()  # Convert to (m,) shape
+        predictions = np.where(A >= 0.5, 1, 0)
         cost = self.cost(Y, A)
 
         return predictions, cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """
+        Calculates one pass of gradient descent on the neural network.
+
+        Args:
+            - X (numpy.ndarray): Input data with shape (nx, m).
+            - Y (numpy.ndarray): Correct labels with shape (1, m).
+            - A1 (numpy.ndarray): Output of the hidden layer.
+            - A2 (numpy.ndarray): Predicted output.
+            - alpha (float): Learning rate.
+
+        Updates:
+            - Private attributes __W1, __b1, __W2, and __b2.
+        """
+        m = Y.shape[1]
+
+        # Backward propagation
+        dZ2 = A2 - Y
+        dW2 = (1 / m) * np.dot(dZ2, A1.T)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+
+        dZ1 = np.dot(self.W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = (1 / m) * np.dot(dZ1, X.T)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+        # Gradient descent update
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """
+        Trains the neural network.
+
+        Args:
+            - X (numpy.ndarray): Input data with shape (nx, m).
+            - Y (numpy.ndarray): Correct labels with shape (1, m).
+            - iterations (int): Number of iterations to train over.
+            - alpha (float): Learning rate.
+
+        Returns:
+            - Tuple of numpy.ndarray and float: Predicted labels and cost.
+        """
+        if not isinstance(iterations, int):
+            raise TypeError('iterations must be an integer')
+        if iterations <= 0:
+            raise ValueError('iterations must be a positive integer')
+        if not isinstance(alpha, float):
+            raise TypeError('alpha must be a float')
+        if alpha <= 0:
+            raise ValueError('alpha must be positive')
+
+        for i in range(iterations):
+            A1, A2 = self.forward_prop(X)
+            self.gradient_descent(X, Y, A1, A2, alpha)
+
+        return self.evaluate(X, Y)
