13f8548e06e193dcbb5851255611b8d3b255fde6
zgamble30 <zgamble30@gmail.com>
2023-10-11 21:39:15 -0500

    Refactor TensorFlow-based neural network training code, removing dependency on NumPy and optimizing imports.

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
-import numpy as np
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_train_op = __import__('5-create_train_op').create_train_op
+
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """
