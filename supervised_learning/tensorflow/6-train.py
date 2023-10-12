129f3ebc31d8f7c57e58b5fe85d1b34a168f53e2
zgamble30 <zgamble30@gmail.com>
2023-10-11 21:53:30 -0500

    last fix

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Training Operation"""
 import tensorflow.compat.v1 as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
@@ -7,7 +8,7 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, 
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
           iterations, save_path="/tmp/model.ckpt"):
 
     """
