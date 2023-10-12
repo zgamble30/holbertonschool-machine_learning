61109abe050eb50651a0f2d2a6ac73fbe02d0762
zgamble30 <zgamble30@gmail.com>
2023-10-11 21:45:12 -0500

    cant figure it out

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,32 +1,27 @@
-#!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
-create_placeholders = __import__('0-create_placeholders').create_placeholders
-forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
-
+forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """
-    Builds, trains, and saves a neural network classifier.
+    Build, train, and save a neural network classifier.
 
-    Args:
-        X_train (numpy.ndarray): Training input data.
-        Y_train (numpy.ndarray): Training labels.
-        X_valid (numpy.ndarray): Validation input data.
-        Y_valid (numpy.ndarray): Validation labels.
-        layer_sizes (list): Number of nodes in each layer of the network.
-        activations (list): Activation functions for each layer.
-        alpha (float): Learning rate.
-        iterations (int): Number of iterations to train over.
-        save_path (str): Path to save the model.
+    Arguments:
+    - X_train: numpy.ndarray containing the training input data
+    - Y_train: numpy.ndarray containing the training labels
+    - X_valid: numpy.ndarray containing the validation input data
+    - Y_valid: numpy.ndarray containing the validation labels
+    - layer_sizes: list containing number of nodes in each layer of the network
+    - activations: list containing activation functions for each layer of the network
+    - alpha: learning rate
+    - iterations: number of iterations to train over
+    - save_path: path to save the model
 
-    Returns:
-        str: Path where the model was saved.
+    Returns: the path where the model was saved
     """
-    # Reset the TensorFlow graph
-    tf.reset_default_graph()
 
     # Create placeholders
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
@@ -34,20 +29,22 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
     # Forward propagation
     y_pred = forward_prop(x, layer_sizes, activations)
 
-    # Calculate accuracy and loss
+    # Calculate accuracy
     accuracy = calculate_accuracy(y, y_pred)
+
+    # Calculate loss
     loss = calculate_loss(y, y_pred)
 
     # Create training operation
     train_op = create_train_op(loss, alpha)
 
-    # Add tensors to graph's collection
-    tf.add_to_collection("x", x)
-    tf.add_to_collection("y", y)
-    tf.add_to_collection("y_pred", y_pred)
-    tf.add_to_collection("loss", loss)
-    tf.add_to_collection("accuracy", accuracy)
-    tf.add_to_collection("train_op", train_op)
+    # Add to the graph's collection
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
 
     # Initialize global variables
     init = tf.global_variables_initializer()
@@ -55,7 +52,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
     # Start session to execute computation graph
     with tf.Session() as sess:
         sess.run(init)
-
         for i in range(iterations + 1):
             t_cost, t_accuracy = sess.run([loss, accuracy],
                                           feed_dict={x: X_train, y: Y_train})
