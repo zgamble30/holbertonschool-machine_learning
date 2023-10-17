aa5c50e4d66dcb5eeebf2df01a255c59d5b8cf53
zgamble30 <zgamble30@gmail.com>
2023-10-17 11:12:06 -0500

    added more comments and broke up a line so it would be pycodestyle

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
-"""Adam"""
+"""
+Adam
+"""
 
 import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
-    """Updates a variable using the Adam optimization algorithm."""
+    """
+    Updates a variable using the Adam optimization algorithm.
+
+    Args:
+        alpha: the learning rate
+        beta1: the weight used for the first moment
+        beta2: the weight used for the second moment
+        epsilon: a small number to avoid division by zero
+        var: numpy.ndarray containing the variable to be updated
+        grad: numpy.ndarray containing the gradient of var
+        v: the previous first moment of var
+        s: the previous second moment of var
+        t: the time step used for bias correction
+
+    Returns:
+        tuple: the updated variable, the new first moment, and the new second moment
+    """
     Vd = beta1 * v + (1 - beta1) * grad
-    Sd = beta2 * s + (1 - beta2) * grad ** 2
-    Vd_corrected = Vd / (1 - beta1 ** t)
-    Sd_corrected = Sd / (1 - beta2 ** t)
-    var_updated = var - alpha * (Vd_corrected / (np.sqrt(Sd_corrected) + epsilon))
+    Sd = beta2 * s + (1 - beta2) * grad**2
+
+    Vd_corrected = Vd / (1 - beta1**t)
+    Sd_corrected = Sd / (1 - beta2**t)
+
+    var_updated = var - alpha * (Vd_corrected /
+                                 (np.sqrt(Sd_corrected) + epsilon))
+
     return var_updated, Vd, Sd
