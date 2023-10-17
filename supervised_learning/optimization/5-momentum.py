e73058250924574ca63bea27d1fdeb51454a8f56
zgamble30 <zgamble30@gmail.com>
2023-10-17 10:52:35 -0500

    fixed formatting

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -3,7 +3,8 @@
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
-    """Updates a variable using the gradient descent with momentum optimization algorithm."""
+    """Updates a variable using the gradient
+    descent with momentum optimization algorithm."""
     v = beta1 * v + (1 - beta1) * grad
     var = var - alpha * v
 
