106964062a6836f09c8b23000c4e04e67669051f
zgamble30 <zgamble30@gmail.com>
2023-10-17 11:36:31 -0500

    added extra line

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -8,7 +8,8 @@ import tensorflow.compat.v1 as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
-    Creates a learning rate decay operation in tensorflow using inverse time decay
+    Creates a learning rate decay
+    operation in tensorflow using inverse time decay
     """
     learning_rate = tf.train.inverse_time_decay(
         alpha, global_step, decay_step, decay_rate, staircase=True
