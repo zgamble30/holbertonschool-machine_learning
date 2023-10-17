21d4af9a5ae20d317acd50a9a934e37e2f004596
zgamble30 <zgamble30@gmail.com>
2023-10-17 10:51:25 -0500

    updates a variable using the gradient descent with momentum optimization algorithm

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Momentum"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """Updates a variable using the gradient descent with momentum optimization algorithm."""
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+
+    return var, v
