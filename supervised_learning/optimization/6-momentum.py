72a83de54f84305998ca41456281a402312516dc
zgamble30 <zgamble30@gmail.com>
2023-10-17 10:54:25 -0500

    creates the training operation for a neural network in tensorflow using the gradient descent with momentum optimization algorithm

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Momentum"""
+
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """Creates the training operation for a neural network in tensorflow
+    using the gradient descent with momentum optimization algorithm."""
+    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1)
+    train_op = optimizer.minimize(loss)
+
+    return train_op
+
