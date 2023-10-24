14bd403c12f65acf2a5a93d62c99b0a1dbb4982e
zgamble30 <zgamble30@gmail.com>
2023-10-24 18:48:32 -0500

    style errors

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -2,14 +2,18 @@
 """calculates precision"""
 import numpy as np
 
+
 def precision(confusion):
     """
     Calculates the precision for each class in a confusion matrix.
     Args:
-        confusion: a numpy.ndarray of shape (classes, classes) representing the confusion matrix
-        where row indices represent the correct labels and column indices represent the predicted labels.
+        confusion: a numpy.ndarray of shape (classes,
+        classes) representing the confusion matrix
+        where row indices represent the correct labels
+        and column indices represent the predicted labels.
     Returns:
-        A numpy.ndarray of shape (classes,) containing the precision of each class.
+        A numpy.ndarray of shape (classes,)
+        containing the precision of each class.
     """
     classes = confusion.shape[0]
     precision_values = np.zeros(classes)
