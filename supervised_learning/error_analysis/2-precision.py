55d1d710cd3e3caafe6e23df638d5ed1a2b7c801
zgamble30 <zgamble30@gmail.com>
2023-10-24 18:44:50 -0500

    added function that calculates precision

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""calculates precision"""
+import numpy as np
+
+def precision(confusion):
+    """
+    Calculates the precision for each class in a confusion matrix.
+    Args:
+        confusion: a numpy.ndarray of shape (classes, classes) representing the confusion matrix
+        where row indices represent the correct labels and column indices represent the predicted labels.
+    Returns:
+        A numpy.ndarray of shape (classes,) containing the precision of each class.
+    """
+    classes = confusion.shape[0]
+    precision_values = np.zeros(classes)
+
+    for i in range(classes):
+        true_positives = confusion[i, i]
+        predicted_positives = np.sum(confusion[:, i])
+        if predicted_positives == 0:
+            precision_values[i] = 0.0
+        else:
+            precision_values[i] = true_positives / predicted_positives
+
+    return precision_values
