011d621de428b267abca3435d36bd48691f6c9ac
zgamble30 <zgamble30@gmail.com>
2023-10-25 17:46:43 -0500

    fixing formatting

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -8,18 +8,27 @@ def specificity(confusion):
     Calculate the specificity for each class in a confusion matrix.
 
     Args:
-        confusion: a numpy.ndarray of shape (classes, classes) where row indices
-                   represent the correct labels and column indices represent the
+        confusion: a numpy.ndarray of shape
+        (classes, classes) where row indices
+                   represent the correct labels
+                   and column indices represent the
                    predicted labels.
 
     Returns:
-        A numpy.ndarray of shape (classes,) containing the specificity of each class.
+        A numpy.ndarray of shape (classes,)
+        containing the specificity of each class.
     """
     classes = confusion.shape[0]
     spec = np.zeros(classes)
 
     for i in range(classes):
-        true_negative = np.sum(confusion) - np.sum(confusion[i, :]) - np.sum(confusion[:, i]) + confusion[i, i]
+        true_negative = (
+            np.sum(confusion) -
+            np.sum(confusion[i, :]) -
+            np.sum(confusion[:, i]) +
+            confusion[i, i]
+            )
+
         actual_negative = np.sum(confusion[:, i]) - confusion[i, i]
         spec[i] = true_negative / (true_negative + actual_negative)
 
