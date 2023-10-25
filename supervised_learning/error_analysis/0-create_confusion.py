690a59cd99c2778865e94e4fa338192aef85f3ba
zgamble30 <zgamble30@gmail.com>
2023-10-24 19:08:54 -0500

    bug fixes. added def

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-"""creates a confusion matrix"""
+"""Creates a confusion matrix"""
 import numpy as np
 
-fusion_matrix(labels, logits):
+
+def create_confusion_matrix(labels, logits):
     """
     Creates a confusion matrix.
+    
     Args:
         labels: one-hot numpy.ndarray of shape (m, classes)
             containing the correct labels for each data point.
         logits: one-hot numpy.ndarray of shape (m, classes)
             containing the predicted labels.
+    
     Returns:
         A confusion numpy.ndarray of shape (classes, classes)
         with row indices representing the correct labels
         and column indices representing the predicted labels.
     """
     m, classes = labels.shape
-    confusion_matrix = np.zeros((classes, classes))
+    confusion_matrix = np.zeros((classes, classes)
 
     for i in range(m):
         true_label = np.argmax(labels[i])
