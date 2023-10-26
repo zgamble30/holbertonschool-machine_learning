432b31b79a3091f57c1f14819f28ed29e5d68708
zgamble30 <zgamble30@gmail.com>
2023-10-26 13:23:45 -0500

    update

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -4,23 +4,32 @@
 import tensorflow.compat.v1 as tf
 
 
-def l2_reg_create_layer(prev, n, activation, lambtha):
-    """creates a TensorFlow layer that includes L2 regularization:"""
+def custom_l2_regularized_layer(previous_layer, num_units, activation, regularization_lambda):
+    """
+    Create a TensorFlow layer that includes L2 regularization:
+    Args:
+        previous_layer: The previous layer to connect to.
+        num_units: The number of units (neurons) in the layer.
+        activation: The activation function to use for the layer.
+        regularization_lambda: The L2 regularization parameter.
+    Returns:
+        A TensorFlow layer with L2 regularization applied.
+    """
     
     # Define an L2 regularization term
-    l2_reg = tf.keras.regularizers.l2(lambtha)
+    l2_regularizer = tf.keras.regularizers.l2(regularization_lambda)
     
     # Initialize weights using Variance Scaling
-    weights_initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+    weights_init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
     
     # Create a dense layer with L2 regularization
-    layer = tf.layers.Dense(
-        n,
+    l2_regularized_layer = tf.layers.Dense(
+        num_units,
         activation=activation,
-        kernel_initializer=weights_initializer,
-        kernel_regularizer=l2_reg,
-        name="layer"
+        kernel_initializer=weights_init,
+        kernel_regularizer=l2_regularizer,
+        name="custom_layer"
     )
     
     # Apply the layer to the previous layer
-    return layer(prev)
+    return l2_regularized_layer(previous_layer)
