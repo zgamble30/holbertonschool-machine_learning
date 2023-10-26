2115b617ad999b154e79ede0fc3d09b39cca4c48
zgamble30 <zgamble30@gmail.com>
2023-10-26 12:54:35 -0500

    tweaks

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
-"""Calculates the cost of a neural network
-with L2 regularization using TensorFlow"""
+"""
+This function calculates the cost of a neural network with L2 regularization.
+"""
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def l2_reg_cost(cost):
     """
-    Calculate the cost of a neural
-    network with L2 regularization.
+    Calculate the cost of a neural network with L2 regularization.
 
     Args:
-        cost: A tensor containing the
-        cost of the network without L2 regularization.
+        cost: A tensor containing the cost of the network without L2 regularization.
 
     Returns:
-        A tensor containing the cost of the
-        network accounting for L2 regularization.
+        A tensor containing the cost of the network accounting for L2 regularization.
     """
-    l2_loss = tf.losses.get_regularization_loss(scope=None)
+    # Get the L2 regularization losses from TensorFlow.
+    l2_loss = tf.losses.get_regularization_losses()
+
+    # Combine the original cost and L2 regularization losses to get the final cost.
     cost_with_l2 = cost + l2_loss
+
     return cost_with_l2
