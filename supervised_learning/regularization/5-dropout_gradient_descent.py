3a76dbdc85000734310464d38f622b46ea3023e9
zgamble30 <zgamble30@gmail.com>
2023-10-26 14:45:06 -0500

    fresh start

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -3,55 +3,35 @@
 
 import numpy as np
 
-
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Updates the weights of a
-    neural network with Dropout regularization
-    using gradient descent.
+    Update the weights of a neural network with Dropout regularization using gradient descent.
 
     Args:
-        Y: The ground truth labels as a
-        numpy.ndarray of shape (output_size, num_samples).
-        weights: Dictionary containing weights and biases for each layer.
-        cache: Dictionary containing
-        the outputs of each layer and dropout masks.
-        alpha: The learning rate for gradient descent.
-        keep_prob: The probability that a node will be kept.
-        L: The number of layers in the neural network.
+        Y (numpy.ndarray): One-hot matrix of shape (classes, m) containing the correct labels for the data.
+        weights (dict): Dictionary of the weights and biases of the neural network.
+        cache (dict): Dictionary of the outputs and dropout masks of each layer of the neural network.
+        alpha (float): The learning rate for gradient descent.
+        keep_prob (float): The probability that a node will be kept.
+        L (int): The number of layers in the network.
 
     Returns:
-        Updated weights and biases.
+        None (weights are updated in place).
     """
+
     m = Y.shape[1]
-    back = {}
+    dZ_output = (cache["A{}".format(L)] - Y)
 
-    for index in range(L, 0, -1):
-        A_current = cache[f"A{index}"]
+    for layer in range(L, 0, -1):
+        A_prev = cache["A{}".format(layer - 1)]
+        dW = np.dot(dZ_output, A_prev.T) / m
+        db = np.sum(dZ_output, axis=1, keepdims=True) / m
 
-        if index == L:
-            # Calculate the error at the output layer
-            back[f"dz{index}"] = A_current - Y
-        else:
-            dz_next = back[f"dz{index + 1}"]
-            W_next = weights[f"W{index + 1}"]
-
-            # Calculate the error for hidden layers
-            dz = np.dot(W_next.T, dz_next) * (1 - np.power(A_current, 2))
-            # tanh derivative
-
-            # Apply dropout mask
-            dz *= cache[f"D{index}"]
-            dz /= keep_prob
-
-            back[f"dz{index}"] = dz
-
-        # Compute gradients
-        dW = (1 / m) * np.dot(dz, cache[f"A{index - 1}"].T)
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+        if layer > 1:
+            dZ_output = np.dot(weights["W{}".format(layer)].T, dZ_output) * (1 - np.power(A_prev, 2))  # tanh derivative
+            dZ_output *= cache["D{}".format(layer - 1)]
+            dZ_output /= keep_prob
 
         # Update weights and biases
-        weights[f"W{index}"] -= alpha * dW
-        weights[f"b{index}"] -= alpha * db
-
-    return weights
+        weights["W{}".format(layer)] -= alpha * dW
+        weights["b{}".format(layer)] -= alpha * db
