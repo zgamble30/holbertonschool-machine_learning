3468251ee1d5971f4d022df547b90252f72a692e
zgamble30 <zgamble30@gmail.com>
2023-10-26 14:49:27 -0500

    fixed style problems

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -3,14 +3,19 @@
 
 import numpy as np
 
+
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Update the weights of a neural network with Dropout regularization using gradient descent.
+    Update the weights of a neural network with
+    Dropout regularization using gradient descent.
 
     Args:
-        Y (numpy.ndarray): One-hot matrix of shape (classes, m) containing the correct labels for the data.
-        weights (dict): Dictionary of the weights and biases of the neural network.
-        cache (dict): Dictionary of the outputs and dropout masks of each layer of the neural network.
+        Y (numpy.ndarray): One-hot matrix of
+        shape (classes, m) containing the correct labels for the data.
+        weights (dict): Dictionary of the
+        weights and biases of the neural network.
+        cache (dict): Dictionary of the
+        outputs and dropout masks of each layer of the neural network.
         alpha (float): The learning rate for gradient descent.
         keep_prob (float): The probability that a node will be kept.
         L (int): The number of layers in the network.
@@ -28,7 +33,8 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         db = np.sum(dZ_output, axis=1, keepdims=True) / m
 
         if layer > 1:
-            dZ_output = np.dot(weights["W{}".format(layer)].T, dZ_output) * (1 - np.power(A_prev, 2))  # tanh derivative
+            dZ_output = np.dot(weights["W{}".format(layer)].T, dZ_output)
+            dZ_output = dZ_output * (1 - np.power(A_prev, 2))
             dZ_output *= cache["D{}".format(layer - 1)]
             dZ_output /= keep_prob
 
