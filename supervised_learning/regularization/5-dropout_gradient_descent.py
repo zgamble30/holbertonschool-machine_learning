6bac2e5200822fb22fd8db548f810dad4b8f21bf
zgamble30 <zgamble30@gmail.com>
2023-10-26 14:34:21 -0500

    Using the derivative of the tanh activation function.

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -30,21 +30,26 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         A_current = cache[f"A{index}"]
 
         if index == L:
-            dz = A_current - Y
+            # Calculate the error at the output layer
+            back[f"dz{index}"] = A_current - Y
         else:
             dz_next = back[f"dz{index + 1}"]
             W_next = weights[f"W{index + 1}"]
 
-            dz = np.dot(W_next.T, dz_next) * (A_current * (1 - A_current))
+            # Calculate the error for hidden layers
+            dz = np.dot(W_next.T, dz_next) * (1 - np.power(A_current, 2))  # tanh derivative
+
+            # Apply dropout mask
             dz *= cache[f"D{index}"]
             dz /= keep_prob
 
-        if index > 1:
             back[f"dz{index}"] = dz
 
+        # Compute gradients
         dW = (1 / m) * np.dot(dz, cache[f"A{index - 1}"].T)
         db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
 
+        # Update weights and biases
         weights[f"W{index}"] -= alpha * dW
         weights[f"b{index}"] -= alpha * db
 
