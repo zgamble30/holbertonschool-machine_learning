dceaba3a738512385c3c5a07c24e1f4a6f297624
zgamble30 <zgamble30@gmail.com>
2023-10-26 23:41:10 -0500

    runniung out of ideas

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.layers import Dropout
+import tensorflow.compat.v1 as tf
 
-def dropout_create_layer(prev, n, activation, keep_prob):
-    reg = Dropout(rate=keep_prob)
-    init = tf.keras.initializers.VarianceScaling(mode="fan_avg")
 
-    layer = tf.keras.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_initializer=init
-    )(prev)
-    output = reg(layer)
+tf.set_random_seed(0)
+dropout_create_layer = __import__('6-dropout_create_layer').dropout_create_layer
 
-    return output
-
+if __name__ == '__main__':
+    np.random.seed(0)
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    X = np.random.randint(0, 256, size=(10, 784))
+    a = dropout_create_layer(x, 256, tf.nn.tanh, 0.8)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print(sess.run(a, feed_dict={x: X}))
