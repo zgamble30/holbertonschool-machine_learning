b73c9681537f2facae40a92f8d52a3d8b856c71f
zgamble30 <zgamble30@gmail.com>
2023-10-26 23:39:18 -0500

    remove tests

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -17,12 +17,3 @@ def dropout_create_layer(prev, n, activation, keep_prob):
 
     return output
 
-if __name__ == '__main__':
-    tf.random.set_seed(0)
-    np.random.seed(0)
-    x = tf.placeholder(tf.float32, shape=[None, 784])
-    X = np.random.randint(0, 256, size=(10, 784))
-    a = dropout_create_layer(x, 256, tf.nn.tanh, 0.8)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        print(sess.run(a, feed_dict={x: X}))
