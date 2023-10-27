330615b56c9e926f9412b292fc6c926fcee38b44
zgamble30 <zgamble30@gmail.com>
2023-10-26 23:26:56 -0500

    ehhh

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -5,10 +5,10 @@ early stopping determined by gradient descent
 
 
 def early_stopping(current_cost,
-                    best_cost,
-                    threshold,
-                    max_patience,
-                    patience_count):
+                   best_cost,
+                   threshold,
+                   max_patience,
+                   patience_count):
 
     """
     A function utilizing Early Stopping Regularization, The Easiest
