db5f35634cec8a60670ea862d4fa6f4d348da119
zgamble30 <zgamble30@gmail.com>
2023-10-31 16:06:19 -0500

    break long lines into small lines

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -6,7 +6,8 @@ import tensorflow.keras as K
 
 def optimize_model(network, alpha, beta1, beta2):
     """
-    Sets up Adam optimization for a keras model with categorical crossentropy loss.
+    Sets up Adam optimization for a keras
+    model with categorical crossentropy loss.
 
     Args:
     - network: the model to optimize.
@@ -16,7 +17,9 @@ def optimize_model(network, alpha, beta1, beta2):
 
     Returns: None.
     """
-    optimizer = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+  optimizer = K.optimizers.Adam(
+    learning_rate=alpha, beta_1=beta1, beta_2=beta2
+)
     network.compile(
         optimizer=optimizer,
         loss='categorical_crossentropy',
