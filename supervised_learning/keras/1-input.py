ae5ace5b9ec02c4082e18914872bba204f1363df
zgamble30 <zgamble30@gmail.com>
2023-10-31 15:42:21 -0500

    function that builds a neural network with the Keras library

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Builds a Keras model with a specified architecture using Input class.
+"""
+import tensorflow.keras as K
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a Keras model with the specified architecture using Input class.
+
+    Args:
+    - nx: number of input features to the network.
+    - layers: list containing the number of nodes in each layer of the network.
+    - activations: list containing the activation functions used for each layer of the network.
+    - lambtha: L2 regularization parameter.
+    - keep_prob: probability that a node will be kept for dropout.
+
+    Returns:
+    A Keras model.
+    """
+    X = K.Input(shape=(nx,))
+    regularizer = K.regularizers.l2(lambtha)
+    A = X
+
+    for i in range(len(layers)):
+        layer = K.layers.Dense(units=layers[i], activation=activations[i],
+                              kernel_regularizer=regularizer)(A)
+
+        dropout = K.layers.Dropout(1 - keep_prob)(layer)
+
+        A = dropout
+
+    model = K.models.Model(inputs=X, outputs=A)
+    return model
