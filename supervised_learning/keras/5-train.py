08bdce48390bd98189561f877c9836bceffbb1d9
zgamble30 <zgamble30@gmail.com>
2023-10-31 16:42:05 -0500

    to also analyze validaiton data

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+# Force Seed - fix for Keras
+SEED = 0
+
+import os
+os.environ['PYTHONHASHSEED'] = str(SEED)
+import random
+random.seed(SEED)
+import numpy as np
+np.random.seed(SEED)
+import tensorflow as tf
+tf.random.set_seed(SEED)
+import tensorflow.keras as K
+
+# Imports
+build_model = __import__('1-input').build_model
+optimize_model = __import__('2-optimize').optimize_model
+one_hot = __import__('3-one_hot').one_hot
+train_model = __import__('5-train').train_model
