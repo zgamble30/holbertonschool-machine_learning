4c6ee9080190c439f8209aa92110006a2d3c3d6a
zgamble30 <zgamble30@gmail.com>
2023-10-31 15:45:50 -0500

    fixing bugs

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -11,7 +11,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     Args:
     - nx: number of input features to the network.
     - layers: list containing the number of nodes in each layer of the network.
-    - activations: list containing the activation functions used for each layer of the network.
+    - activations: list containing the
+    activation functions used for each layer of the network.
     - lambtha: L2 regularization parameter.
     - keep_prob: probability that a node will be kept for dropout.
 
@@ -26,9 +27,11 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         layer = K.layers.Dense(units=layers[i], activation=activations[i],
                               kernel_regularizer=regularizer)(A)
 
-        dropout = K.layers.Dropout(1 - keep_prob)(layer)
-
-        A = dropout
+        if i < len(layers) - 1 and keep_prob < 1.0:
+            dropout = K.layers.Dropout(1 - keep_prob)(layer)
+            A = dropout
+        else:
+            A = layer
 
     model = K.models.Model(inputs=X, outputs=A)
     return model
