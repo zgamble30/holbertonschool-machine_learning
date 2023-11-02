d5408cf525bf12c413bfb122ba7779547c3ce7cb
zgamble30 <zgamble30@gmail.com>
2023-11-01 19:34:27 -0500

    function that makes a prediciton

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+makes prediction
+"""
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+    Makes a prediction using a neural network.
+
+    Args:
+        - network: the network model to make the prediction with.
+        - data: the input data to make the prediction with.
+        - verbose: a boolean that determines if
+        output should be printed during the prediction process.
+    
+    Returns:
+        - the prediction for the data.
+    """
+    prediction = network.predict(data, verbose=verbose)
+    return prediction
