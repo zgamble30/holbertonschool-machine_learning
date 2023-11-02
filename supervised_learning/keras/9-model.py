c633337baf2c7a0a42c2ada147be1cecca0974b7
zgamble30 <zgamble30@gmail.com>
2023-11-01 20:03:28 -0500

    loads and saves models

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+saves and loads models
+"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    Save an entire Keras model to a file.
+
+    Args:
+        network (tf.keras.Model): The model to save.
+        filename (str): The path to the file
+        where the model should be saved.
+
+    Returns:
+        None
+    """
+    network.save(filename)
+
+def load_model(filename):
+    """
+    Load an entire Keras model from a file.
+
+    Args:
+        filename (str): The path to the file
+        from which to load the model.
+
+    Returns:
+        tf.keras.Model: The loaded Keras model.
+    """
+    loaded_model = K.models.load_model(filename)
+    return loaded_model
