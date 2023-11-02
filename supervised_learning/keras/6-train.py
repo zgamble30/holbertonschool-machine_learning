9a81b3e785635aa8485c7d2751d8cf400847f146
zgamble30 <zgamble30@gmail.com>
2023-11-01 19:22:21 -0500

    cleaned up style

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -14,19 +14,27 @@ def train_model(network, data, labels, batch_size, epochs,
 
     Args:
         - network: the model to train.
-        - data: a numpy.ndarray of shape (m, nx) containing the input data.
+        - data: a numpy.ndarray of shape (m, nx)
+        containing the input data.
         - labels: a one-hot numpy.ndarray of shape
           (m, classes) containing the labels of data.
-        - batch_size: the size of the batch used for mini-batch gradient descent.
-        - epochs: the number of passes through data for mini-batch gradient descent.
+        - batch_size: the size of the batch used
+        for mini-batch gradient descent.
+        - epochs: the number of passes through
+        data for mini-batch gradient descent.
         - validation_data: a tuple (X_valid, Y_valid)
-          containing validation data, where X_valid is the input data
+          containing validation data,
+          where X_valid is the input data
           and Y_valid is the labels for validation. Default is None.
         - early_stopping: a boolean that indicates
-          whether early stopping should be used. Default is False.
-        - patience: the patience used for early stopping. Default is 0.
-        - verbose: a boolean that determines if output should be printed during training.
-        - shuffle: a boolean that determines whether to shuffle the batches every epoch.
+          whether early stopping should be
+          used. Default is False.
+        - patience: the patience used for early
+        stopping. Default is 0.
+        - verbose: a boolean that determines if output
+        should be printed during training.
+        - shuffle: a boolean that determines
+        whether to shuffle the batches every epoch.
 
     Returns:
         - the History object generated after training the model.
