079198b91292285b64c960c8f96b6b72f992f5d0
zgamble30 <zgamble30@gmail.com>
2023-11-02 12:27:33 -0500

    reformatting

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -24,27 +24,37 @@ def convolve_grayscale_padding(images, kernel, padding):
     It performs a convolution while applying the specified padding.
     The result is a numpy array containing the convolved images.
     """
-    num_images = images.shape[0]
-    image_height = images.shape[1]
-    image_width = images.shape[2]
-    kernel_height = kernel.shape[0]
-    kernel_width = kernel.shape[1]
+    num_images, image_height, image_width = images.shape
+    kernel_height, kernel_width = kernel.shape
     padding_height, padding_width = padding
 
     # Pad the images with zeros
     padded_images = np.pad(
-        images, ((0, 0), (padding_height, padding_height), (padding_width, padding_width)),
+        images,
+        ((0, 0), (padding_height, padding_height),
+         (padding_width, padding_width)),
         'constant'
     )
 
     # Calculate the dimensions of the convolved images
-    convolved_width = image_width - kernel_width + 1 + (2 * padding_width)
-    convolved_height = image_height - kernel_height + 1 + (2 * padding_height)
-    convolved_images = np.zeros((num_images, convolved_height, convolved_width))
+    convolved_width = (
+        image_width - kernel_width + 1 + 2 * padding_width
+    )
+    convolved_height = (
+        image_height - kernel_height + 1 + 2 * padding_height
+    )
+
+    convolved_images = np.zeros(
+        (num_images, convolved_height, convolved_width)
+    )
 
     for x in range(convolved_width):
         for y in range(convolved_height):
-            image_section = padded_images[:, y:y + kernel_height, x:x + kernel_width]
-            convolved_images[:, y, x] = np.tensordot(image_section, kernel, axes=2)
+            image_section = padded_images[
+                :, y:y + kernel_height, x:x + kernel_width
+            ]
+            convolved_images[:, y, x] = np.tensordot(
+                image_section, kernel, axes=2
+            )
 
     return convolved_images
