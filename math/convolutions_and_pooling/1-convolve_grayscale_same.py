98530ccca51645c603cb46728a4d51b56dfbc39f
zgamble30 <zgamble30@gmail.com>
2023-11-02 11:35:12 -0500

    functiuon that performs same convulution

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Same convolution on grayscale images"""
+"""Performs a same convolution on grayscale images"""
 import numpy as np
 
 
@@ -8,27 +8,17 @@ def convolve_grayscale_same(images, kernel):
     Perform a same convolution on grayscale images using a given kernel.
 
     Args:
-    - images (numpy.ndarray): A collection of grayscale images with
-    shape (num_images, image_height, image_width).
-    - kernel (numpy.ndarray): The convolution kernel with
-    shape (kernel_height, kernel_width).
+        - images (numpy.ndarray): A collection of grayscale
+          images with shape (num_images, image_height, image_width).
+        - kernel (numpy.ndarray): The convolution kernel with shape
+          (kernel_height, kernel_width).
 
     Returns:
-    - numpy.ndarray: An array containing the convolved images.
+        - numpy.ndarray: An array containing the convolved images.
 
-    The function takes a set of grayscale
-    images and a convolution kernel as input.
-    It performs a same convolution, meaning the
-    output size is the same as the input.
+    The function takes a set of grayscale images and a convolution kernel as input.
+    It performs a same convolution, meaning the output size is the same as the input.
     The result is a numpy array containing the convolved images.
-
-    Example:
-    >>> images = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
-    >>> kernel = np.array([[1, 0], [0, -1]])
-    >>> convolve_grayscale_same(images, kernel)
-    array([[[0,  0,  0],
-            [0,  1,  2],
-            [0,  4,  5]]])
     """
     num_images = images.shape[0]
     image_height = images.shape[1]
@@ -36,25 +26,21 @@ def convolve_grayscale_same(images, kernel):
     kernel_height = kernel.shape[0]
     kernel_width = kernel.shape[1]
 
-    # Calculate padding sizes
-    pad_height = kernel_height - 1
-    pad_width = kernel_width - 1
+    # Calculate padding
+    pad_height = kernel_height // 2
+    pad_width = kernel_width // 2
 
-    # Calculate output size
-    output_height = image_height
-    output_width = image_width
+    # Pad the images
+    padded_images = np.pad(
+        images, ((0, 0), (pad_height, pad_height), (pad_width, pad_width))
+    )
 
-    # Pad the images with zeros
-    pad_values = ((0, 0), (pad_height, pad_height), (pad_width, pad_width))
-    padded_images = np.pad(images, pad_values, mode='constant')
+    convolved_images = np.zeros((num_images, image_height, image_width))
 
-    # Initialize the output array
-    convolved_images = np.zeros((num_images, output_height, output_width))
-
-    # Perform convolution
-    for x in range(output_width):
-        for y in range(output_height):
+    for x in range(image_width):
+        for y in range(image_height):
             image_section = padded_images[:, y:y + kernel_height, x:x + kernel_width]
-            convolved_images[:, y, x] = np.tensordot(image_section, kernel, axes=2)
+            if image_section.shape[1:] == kernel.shape:
+                convolved_images[:, y, x] = np.tensordot(image_section, kernel, axes=2)
 
     return convolved_images
