6f133556b2a57cb671947b49598b1588594e4685
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-11-02 14:00:31 -0500

    Update 2-convolve_grayscale_padding.py

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,60 +1,61 @@
 #!/usr/bin/env python3
-"""Convolution with Padding on Grayscale Images"""
+"""Convolution on Grayscale Images with Padding, Stride, and Same/Valid Options"""
 
 import numpy as np
 
-
-def convolve_grayscale_padding(images, kernel, padding):
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """
-    Perform a convolution on grayscale images with custom padding.
+    Perform a convolution on multi-channel images with customizable options.
 
     Args:
-    - images (numpy.ndarray): A collection of grayscale
-    images with shape (num_images, image_height, image_width).
-    - kernel (numpy.ndarray): The convolution
-    kernel with shape (kernel_height, kernel_width).
-    - padding (tuple): A tuple of (padding_height, padding_width).
+    - images (numpy.ndarray): A collection of multi-channel images with shape
+      (num_images, image_height, image_width, num_channels).
+    - kernel (numpy.ndarray): The convolution kernel with shape
+      (kernel_height, kernel_width, num_channels).
+    - padding (str or tuple): Specifies the padding type. Use 'same' for
+      same padding, 'valid' for no padding, or provide a tuple (ph, pw)
+      for custom padding.
+    - stride (tuple): Specifies the stride (sh, sw) for convolution.
+      Default is (1, 1).
 
     Returns:
-    - numpy.ndarray: An array containing the
-    convolved images.
+    - numpy.ndarray: An array containing the convolved multi-channel images.
 
-    The function takes a set of grayscale images,
-    a convolution kernel, and custom padding values as input.
-    It performs a convolution while applying the specified padding.
-    The result is a numpy array containing the convolved images.
+    The function takes a set of multi-channel images and a convolution kernel
+    as input. It performs a convolution with customizable options,
+    including padding, stride, and 'same'/'valid' modes.
+    The result is a numpy array containing the convolved multi-channel images.
     """
-    num_images, image_height, image_width = images.shape
-    kernel_height, kernel_width = kernel.shape
-    padding_height, padding_width = padding
+    num_images, image_height, image_width, num_channels = images.shape
+    kernel_height, kernel_width, num_channels_kernel = kernel.shape
+    sh, sw = stride
 
-    # Pad the images with zeros
-    padded_images = np.pad(
-        images,
-        ((0, 0), (padding_height, padding_height),
-         (padding_width, padding_width)),
-        'constant'
-    )
+    if padding == 'same':
+        ph = ((((image_height - 1) * sh)
+        + kernel_height - image_height) // 2) + 1
+        pw = ((((image_width - 1) * sw)
+        + kernel_width - image_width) // 2) + 1
+    elif padding == 'valid':
+        ph = 0
+        pw = 0
+    elif type(padding) == tuple:
+        ph, pw = padding
 
-    # Calculate the dimensions of the convolved images
-    convolved_width = (
-        image_width - kernel_width + 1 + 2 * padding_width
-    )
-    convolved_height = (
-        image_height - kernel_height + 1 + 2 * padding_height
-    )
+    convolved_width = ((image_width + (2 * pw) - kernel_width) // sw) + 1
+    convolved_height = ((image_height + (2 * ph) - kernel_height) // sh) + 1
 
-    convolved_images = np.zeros(
+    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    convolved_matrix = np.zeros(
         (num_images, convolved_height, convolved_width)
     )
 
-    for x in range(convolved_width):
-        for y in range(convolved_height):
+    for i in range(convolved_width):
+        for j in range(convolved_height):
             image_section = padded_images[
-                :, y:y + kernel_height, x:x + kernel_width
+                :, sh * j:sh * j + kernel_height, sw * i:sw * i + kernel_width, :
             ]
-            convolved_images[:, y, x] = np.tensordot(
-                image_section, kernel, axes=2
+            convolved_matrix[:, j, i] = np.tensordot(
+                image_section, kernel, axes=3
             )
 
-    return convolved_images
+    return convolved_matrix
