34d97b2249df90fe66f9c3503c267b0d30c501de
zgamble30 <zgamble30@gmail.com>
2023-11-02 10:49:45 -0500

    pycodestyle

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -2,21 +2,27 @@
 """convolution on grayscale images"""
 import numpy as np
 
-
 def convolve_grayscale_valid(images, kernel):
     """
-    Perform a valid convolution on grayscale images using a given kernel.
+    Perform a valid convolution on
+    grayscale images using a given kernel.
 
     Args:
-    - images (numpy.ndarray): A collection of grayscale images with shape (num_images, image_height, image_width).
-    - kernel (numpy.ndarray): The convolution kernel with shape (kernel_height, kernel_width).
+    - images (numpy.ndarray): A collection of grayscale
+    images with shape (num_images, image_height, image_width).
+    - kernel (numpy.ndarray): The convolution
+    kernel with shape (kernel_height, kernel_width).
 
     Returns:
-    - numpy.ndarray: An array containing the convolved images.
+    - numpy.ndarray: An array containing
+    the convolved images.
     
-    The function takes a set of grayscale images and a convolution kernel as input.
-    It performs a valid convolution, meaning the output size is reduced compared to the input.
-    The result is a numpy array containing the convolved images.
+    The function takes a set of grayscale images
+    and a convolution kernel as input.
+    It performs a valid convolution, meaning the
+    output size is reduced compared to the input.
+    The result is a numpy array containing the
+    convolved images.
 
     Example:
     >>> images = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
@@ -26,7 +32,7 @@ def convolve_grayscale_valid(images, kernel):
             [ 4,  5],
             [ 7,  8]]])
     """
-    num_images = images.shape[0]
+    num_images = images.shape[0
     image_height = images.shape[1]
     image_width = images.shape[2]
     kernel_height = kernel.shape[0]
@@ -39,6 +45,8 @@ def convolve_grayscale_valid(images, kernel):
     for x in range(output_width):
         for y in range(output_height):
             image_section = images[:, y:y + kernel_height, x:x + kernel_width]
-            convolved_images[:, y, x] = np.tensordot(image_section, kernel, axes=2)
+            convolved_images[:, y, x] = np.tensordot(
+                image_section, kernel, axes=2
+            )
 
     return convolved_images
