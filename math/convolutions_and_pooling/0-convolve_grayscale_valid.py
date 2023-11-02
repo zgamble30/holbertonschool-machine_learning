4027b71532db18904a888fb3bbe66341e839d156
zgamble30 <zgamble30@gmail.com>
2023-11-02 10:45:27 -0500

    function that uses convolution on grayscale images

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
-"""grayscale convolution"""
+"""convolution on grayscale images"""
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
     """
-    Perform a valid convolution on grayscale images.
+    Perform a valid convolution on grayscale images using a given kernel.
 
     Args:
-        images: numpy.ndarray with shape (m, h, w) containing multiple grayscale images.
-        kernel: numpy.ndarray with shape (kh, kw) containing the kernel for convolution.
+    - images (numpy.ndarray): A collection of grayscale images with shape (num_images, image_height, image_width).
+    - kernel (numpy.ndarray): The convolution kernel with shape (kernel_height, kernel_width).
 
     Returns:
-        A numpy.ndarray containing the convolved images.
+    - numpy.ndarray: An array containing the convolved images.
+    
+    The function takes a set of grayscale images and a convolution kernel as input.
+    It performs a valid convolution, meaning the output size is reduced compared to the input.
+    The result is a numpy array containing the convolved images.
+
+    Example:
+    >>> images = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
+    >>> kernel = np.array([[1, 0], [0, -1]])
+    >>> convolve_grayscale_valid(images, kernel)
+    array([[[ 1,  2],
+            [ 4,  5],
+            [ 7,  8]]])
     """
-    m, h, w = images.shape
-    kh, kw = kernel.shape
-    output_h = h - kh + 1
-    output_w = w - kw + 1
-    output = np.zeros((m, output_h, output_w)
+    num_images = images.shape[0]
+    image_height = images.shape[1]
+    image_width = images.shape[2]
+    kernel_height = kernel.shape[0]
+    kernel_width = kernel.shape[1]
 
-    for i in range(output_h):
-        for j in range(output_w):
-            # Extract the region of interest from images and perform element-wise multiplication
-            # Then, sum over axes (1, 2) to get the convolved value
-            conv_result = np.sum(images[:, i:i + kh, j:j + kw] * kernel, axis=(1, 2)
-            output[:, i, j] = conv_result
+    output_width = image_width - kernel_width + 1
+    output_height = image_height - kernel_height + 1
+    convolved_images = np.zeros((num_images, output_height, output_width))
 
-    return output
+    for x in range(output_width):
+        for y in range(output_height):
+            image_section = images[:, y:y + kernel_height, x:x + kernel_width]
+            convolved_images[:, y, x] = np.tensordot(image_section, kernel, axes=2)
+
+    return convolved_images
