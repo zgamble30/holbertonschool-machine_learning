152cbcbdea7bf02896209b8b37e9c6665c96f01e
zgamble30 <zgamble30@gmail.com>
2023-11-01 19:46:26 -0500

    save and load config functions

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+evaluates network
+"""
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """
+    Saves a model's configuration in a text file.
+
+    Args:
+        - network: the model whose configuration
+        should be saved.
+        - filename: the path of the file that
+        the configuration should be saved to.
+
+    Returns: None
+    """
+    config = network.to_json()
+    with open(filename, 'w') as file:
+        file.write(config)
+
+def load_config(filename):
+    """
+    Loads a model with a specific configuration.
+
+    Args:
+        - filename: the path of the file
+        containing the model's configuration.
+
+    Returns: the loaded model
+    """
+    with open(filename, 'r') as file:
+        config = file.read()
+    loaded_model = K.models.model_from_json(config)
+    return loaded_model
