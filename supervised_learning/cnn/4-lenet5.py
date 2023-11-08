e6e5dd41339ce4c2521223661ea20765b3003c65
zgamble30 <zgamble30@gmail.com>
2023-11-08 15:57:05 -0600

    formatting issues

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -22,15 +22,25 @@ def lenet5(x, y):
     initializer = tf.keras.initializers.VarianceScaling(scale=2.0)
 
     # Convolutional Layer 1
-    conv1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
-                             activation=tf.nn.relu, kernel_initializer=initializer)(x)
+    conv1 = tf.layers.Conv2D(
+        filters=6, 
+        kernel_size=(5, 5), 
+        padding='same',
+        activation=tf.nn.relu, 
+        kernel_initializer=initializer
+    )(x)
 
     # Max Pooling Layer 1
     pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
 
     # Convolutional Layer 2
-    conv2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid',
-                             activation=tf.nn.relu, kernel_initializer=initializer)(pool1)
+    conv2 = tf.layers.Conv2D(
+        filters=16, 
+        kernel_size=(5, 5), 
+        padding='valid',
+        activation=tf.nn.relu, 
+        kernel_initializer=initializer
+    )(pool1)
 
     # Max Pooling Layer 2
     pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
@@ -39,13 +49,24 @@ def lenet5(x, y):
     flatten = tf.layers.Flatten()(pool2)
 
     # Fully Connected Layer 1
-    fc1 = tf.layers.Dense(units=120, activation=tf.nn.relu, kernel_initializer=initializer)(flatten)
+    fc1 = tf.layers.Dense(
+        units=120, 
+        activation=tf.nn.relu, 
+        kernel_initializer=initializer
+    )(flatten)
 
     # Fully Connected Layer 2
-    fc2 = tf.layers.Dense(units=84, activation=tf.nn.relu, kernel_initializer=initializer)(fc1)
+    fc2 = tf.layers.Dense(
+        units=84, 
+        activation=tf.nn.relu, 
+        kernel_initializer=initializer
+    )(fc1)
 
     # Output Layer
-    logits = tf.layers.Dense(units=10, kernel_initializer=initializer)(fc2)
+    logits = tf.layers.Dense(
+        units=10, 
+        kernel_initializer=initializer
+    )(fc2)
 
     # Softmax Activation
     y_pred = tf.nn.softmax(logits)
