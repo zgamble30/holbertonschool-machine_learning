e6c4b51705f3f44f3cb7ec66fcc5cf6f2b7a6d9c
zgamble30 <zgamble30@gmail.com>
2023-11-14 15:57:58 -0600

    small fix

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -6,11 +6,11 @@ import tensorflow.keras as K
 def inception_block(A_prev, filters):
     """
     Build an inception block.
-    
+
     Args:
     - A_prev: Output from the previous layer
     - filters: Tuple or list containing F1, F3R, F3, F5R, F5, FPP
-    
+
     Returns:
     - Concatenated output of the inception block
     """
