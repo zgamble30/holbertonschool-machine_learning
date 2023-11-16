39e021a91e6eb6fd99fc4d6f03e9e7b946926ba8
zgamble30 <zgamble30@gmail.com>
2023-11-15 18:36:12 -0600

    reformat

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -5,6 +5,7 @@ Defines the projection_block function
 
 import tensorflow.keras as K
 
+
 def projection_block(A_prev, filters, s=2):
     """
     Builds a projection block as described in Deep Residual Learning for
@@ -22,28 +23,42 @@ def projection_block(A_prev, filters, s=2):
     Returns:
         Activated output of the projection block
     """
-
     # Retrieve filters
     F11, F3, F12 = filters
 
     # Shortcut path
-    shortcut = K.layers.Conv2D(F12, kernel_size=(1, 1), strides=(s, s),
-                               kernel_initializer='he_normal')(A_prev)
+    shortcut = K.layers.Conv2D(
+        F12,
+        kernel_size=(1, 1),
+        strides=(s, s),
+        kernel_initializer='he_normal'
+    )(A_prev)
     shortcut = K.layers.BatchNormalization(axis=3)(shortcut)
 
     # Main path
-    conv1 = K.layers.Conv2D(F11, kernel_size=(1, 1), strides=(s, s),
-                             kernel_initializer='he_normal')(A_prev)
+    conv1 = K.layers.Conv2D(
+        F11,
+        kernel_size=(1, 1),
+        strides=(s, s),
+        kernel_initializer='he_normal'
+    )(A_prev)
     conv1 = K.layers.BatchNormalization(axis=3)(conv1)
     conv1 = K.layers.Activation('relu')(conv1)
 
-    conv2 = K.layers.Conv2D(F3, kernel_size=(3, 3), padding='same',
-                             kernel_initializer='he_normal')(conv1)
+    conv2 = K.layers.Conv2D(
+        F3,
+        kernel_size=(3, 3),
+        padding='same',
+        kernel_initializer='he_normal'
+    )(conv1)
     conv2 = K.layers.BatchNormalization(axis=3)(conv2)
     conv2 = K.layers.Activation('relu')(conv2)
 
-    conv3 = K.layers.Conv2D(F12, kernel_size=(1, 1),
-                             kernel_initializer='he_normal')(conv2)
+    conv3 = K.layers.Conv2D(
+        F12,
+        kernel_size=(1, 1),
+        kernel_initializer='he_normal'
+    )(conv2)
     conv3 = K.layers.BatchNormalization(axis=3)(conv3)
 
     # Add shortcut to main path
