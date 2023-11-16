b3b4b773d6ecdd9b0b3de98523b1eb5ee30e6234
zgamble30 <zgamble30@gmail.com>
2023-11-15 20:58:30 -0600

    trying to fix error with ccn

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -1,79 +1,60 @@
 #!/usr/bin/env python3
 """
-Builds the ResNet-50 architecture as described
-    in Deep Residual Learning for Image Recognition (2015)
+Builds a dense block as described in Densely Connected Convolutional Networks
 """
 import tensorflow.keras as K
-identity_block = __import__('2-identity_block').identity_block
-projection_block = __import__('3-projection_block').projection_block
 
 
-def resnet50():
+def dense_block(X, nb_filters, growth_rate, layers):
     """
-    Builds the ResNet-50 architecture.
+    Builds a dense block as described in Densely Connected Convolutional Networks
+
+    Arguments:
+    - X: output from the previous layer
+    - nb_filters: integer representing the number of filters in X
+    - growth_rate: growth rate for the dense block
+    - layers: number of layers in the dense block
 
     Returns:
-        Keras Model: The ResNet-50 model.
+    - The concatenated output of each layer within the Dense Block
+    - The number of filters within the concatenated outputs, respectively
     """
-    # Function for He normal initialization
-    he_normal_initializer = K.initializers.he_normal()
+    HeNormal = K.initializers.he_normal()
 
-    # Input layer
-    input_layer = K.layers.Input((224, 224, 3))
+    # List to store the concatenated outputs
+    concat_outputs = [X]
 
-    # Initial convolution layer
-    X = K.layers.Conv2D(
-        filters=64,
-        kernel_size=7,
-        strides=2,
-        padding="same",
-        kernel_initializer=he_normal_initializer,
-    )(input_layer)
-    # Batch normalization before activation function
-    X = K.layers.BatchNormalization(axis=3)(X)
-    X = K.layers.Activation('relu')(X)
-    # Max pooling
-    X = K.layers.MaxPooling2D(
-        pool_size=3,
-        strides=2,
-        padding="same",
-    )(X)
+    for _ in range(layers):
+        # Batch Normalization
+        X = K.layers.BatchNormalization(axis=3)(X)
+        # ReLU activation
+        X = K.layers.Activation('relu')(X)
+        # Convolution with bottleneck layer
+        bottleneck = K.layers.Conv2D(
+            filters=growth_rate * 4,
+            kernel_size=1,
+            strides=1,
+            padding='same',
+            kernel_initializer=HeNormal
+        )(X)
+        # Batch Normalization
+        bottleneck = K.layers.BatchNormalization(axis=3)(bottleneck)
+        # ReLU activation
+        bottleneck = K.layers.Activation('relu')(bottleneck)
+        # Convolution
+        conv = K.layers.Conv2D(
+            filters=growth_rate,
+            kernel_size=3,
+            strides=1,
+            padding='same',
+            kernel_initializer=HeNormal
+        )(bottleneck)
+        # Concatenate the output of the current layer to the list
+        concat_outputs.append(conv)
+        # Update the number of filters
+        nb_filters += growth_rate
 
-    # Conv2 block
-    X = projection_block(X, [64, 64, 256], s=1)
-    X = identity_block(X, [64, 64, 256])
-    X = identity_block(X, [64, 64, 256])
+    # Concatenate all the outputs along the last axis
+    concat_block = K.layers.concatenate(concat_outputs, axis=3)
 
-    # Conv3 block
-    X = projection_block(X, [128, 128, 512], s=2)
-    X = identity_block(X, [128, 128, 512])
-    X = identity_block(X, [128, 128, 512])
-    X = identity_block(X, [128, 128, 512])
-
-    # Conv4 block
-    X = projection_block(X, [256, 256, 1024], s=2)
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-    X = identity_block(X, [256, 256, 1024])
-
-    # Conv5 block
-    X = projection_block(X, [512, 512, 2048], s=2)
-    X = identity_block(X, [512, 512, 2048])
-    X = identity_block(X, [512, 512, 2048])
-
-    # Average pooling
-    X = K.layers.AveragePooling2D(
-        pool_size=7,
-        strides=1,
-        padding="valid",
-    )(X)
-
-    # Fully connected layer
-    X = K.layers.Dense(units=1000, activation='softmax',
-                       kernel_initializer=he_normal_initializer)(X)
-
-    # Create and return the model
-    model = K.models.Model(inputs=input_layer, outputs=X, name='ResNet50')
-    return model
+    return concat_block, nb_filters
