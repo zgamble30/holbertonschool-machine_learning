a8de8d8baa93eabd9b5d0b7a87ef43ec3e05e1e6
zgamble30 <zgamble30@gmail.com>
2023-11-22 12:32:08 -0600

    small changes

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -1,70 +1,67 @@
 #!/usr/bin/env python3
 """
-Script to train a convolutional neural network to classify the CIFAR 10 dataset
+Trains a convolutional neural network to classify CIFAR 10 dataset
 """
 import tensorflow.keras as K
 
 
 def preprocess_data(X, Y):
     """
-    Script must save your trained model in the current working
-    directory as cifar10.h5
-    Saved model should be compiled
-    Saved model should have a validation accuracy of 87% or higher
-    Script should not run when the file is imported
-
-    Pre-processes the data using:
-
-    X is a numpy.ndarray of shape (m, 32, 32, 3) containing the CIFAR 10 data,
-    where m is the number of data points
-    Y is a numpy.ndarray of shape (m,) containing the CIFAR 10 labels for X
-    Returns: X_p, Y_p
-        X_p is a numpy.ndarray containing the preprocessed X
-        Y_p is a numpy.ndarray containing the preprocessed Y
-
+    Preprocesses the data for the model
     """
-    X_p = K.applications.efficientnet_v2.preprocess_input(
-        X, data_format="channels_last")
+    # Preprocess input data using VGG16 preprocessing
+    X_p = K.applications.vgg16.preprocess_input(X)
+    # One-hot encode the labels
     Y_p = K.utils.to_categorical(Y, 10)
-    return (X_p, Y_p)
-
+    return X_p, Y_p
 
 if __name__ == '__main__':
     """
-    Trains a convolutional neural network to classify CIFAR 10 dataset
+    Trains a convolutional neural network on CIFAR 10 dataset
     Saves model to cifar10.h5
     """
-    (X_train, Y_train), (X_test, Y_test) = K.datasets.cifar10.load_data()
-    X_train, Y_train = preprocess_data(X_train, Y_train)
-    X_test, Y_test = preprocess_data(X_test, Y_test)
+    # Load CIFAR-10 dataset
+    (X_train_orig, Y_train_orig), (X_test_orig, Y_test_orig) = K.datasets.cifar10.load_data()
+    
+    # Preprocess the training and testing data
+    X_train, Y_train = preprocess_data(X_train_orig, Y_train_orig)
+    X_test, Y_test = preprocess_data(X_test_orig, Y_test_orig)
 
-    inputs = K.Input(shape=(32, 32, 3))
-    inputs_resized = K.layers.Lambda(
+    # Resize images to the input size expected by the chosen application
+    input_resized = K.layers.Lambda(
         lambda x: K.backend.resize_images(x,
-                                          height_factor=(224 // 32),
-                                          width_factor=(224 // 32),
-                                          data_format="channels_last"))(inputs)
+                                          height_factor=(224 / 32),
+                                          width_factor=(224 / 32),
+                                          data_format="channels_last"))(X_train)
 
-    EfficientNetV2B3 = K.applications.EfficientNetV2B3(
-        include_top=False, weights='imagenet',
-        input_shape=(224, 224, 3))
+    # Choose a Keras Application from Keras Applications
+    base_model = K.applications.VGG16(weights='imagenet',
+                                      include_top=False,
+                                      input_tensor=input_resized,
+                                      input_shape=(224, 224, 3))
 
-    X = EfficientNetV2B3(inputs_resized, training=False)
-    X = K.layers.Flatten()(X)
-    outputs = K.layers.Dense(10, activation='softmax')(X)
+    # Freeze the layers in the base model
+    for layer in base_model.layers:
+        layer.trainable = False
 
-    model = K.Model(inputs=inputs, outputs=outputs)
+    # Add custom layers for classification on top of the base model
+    x = K.layers.Flatten()(base_model.output)
+    x = K.layers.Dense(256, activation='relu')(x)
+    output_layer = K.layers.Dense(10, activation='softmax')(x)
 
-    EfficientNetV2B3.trainable = False
+    # Create a new model that includes both the base model and the custom layers
+    model = K.models.Model(inputs=base_model.input, outputs=output_layer)
 
+    # Compile the model with the specified loss function, optimizer, and metrics
     model.compile(loss='categorical_crossentropy',
                   optimizer=K.optimizers.Adam(),
                   metrics=['accuracy'])
 
+    # Train the model on the preprocessed data
     history = model.fit(x=X_train, y=Y_train,
                         validation_data=(X_test, Y_test),
-                        batch_size=600,
-                        epochs=7)
+                        batch_size=128,
+                        epochs=10)
 
+    # Save the trained model
     model.save('cifar10.h5')
-    
