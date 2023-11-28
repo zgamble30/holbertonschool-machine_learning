e372cf5a65bfecb758732d5ea6033e79de96f5ac
zgamble30 <zgamble30@gmail.com>
2023-11-28 16:23:05 -0600

    preprocess data and define layers

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -19,3 +19,19 @@ def preprocess_data(X, Y):
         X, data_format="channels_last")
     Y_preprocessed = K.utils.to_categorical(Y, 10)
     return X_preprocessed, Y_preprocessed
+
+if __name__ == '__main__':
+    # Load CIFAR-10 dataset
+    (X_train_raw, Y_train_raw), (X_test_raw, Y_test_raw) = K.datasets.cifar10.load_data()
+    
+    # Preprocess the data
+    X_train, Y_train = preprocess_data(X_train_raw, Y_train_raw)
+    X_test, Y_test = preprocess_data(X_test_raw, Y_test_raw)
+
+    # Define input layer and resize images
+    inputs = K.Input(shape=(32, 32, 3))
+    inputs_resized = K.layers.Lambda(
+        lambda x: K.backend.resize_images(x,
+                                          height_factor=(224 // 32),
+                                          width_factor=(224 // 32),
+                                          data_format="channels_last"))(inputs)
