300527464aeab93d0478bbe612e7df156cf8d77b
zgamble30 <zgamble30@gmail.com>
2023-12-07 10:01:11 -0600

    reformatting

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -36,7 +36,6 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
-
     def load_classes(self, file_path):
         """
         Load and return the list of class names from the specified file.
@@ -49,17 +48,3 @@ class Yolo:
         """
         with open(file_path, 'r') as f:
             return [line.strip() for line in f]
-
-if __name__ == '__main__':
-    import numpy as np
-
-    np.random.seed(0)
-    anchors = np.array([[[116, 90], [156, 198], [373, 326]],
-                        [[30, 61], [62, 45], [59, 119]],
-                        [[10, 13], [16, 30], [33, 23]]])
-    yolo = Yolo('../data/yolo.h5', '../data/coco_classes.txt', 0.6, 0.5, anchors)
-    yolo.model.summary()
-    print('Class names:', yolo.class_names)
-    print('Class threshold:', yolo.class_t)
-    print('NMS threshold:', yolo.nms_t)
-    print('Anchor boxes:', yolo.anchors)
